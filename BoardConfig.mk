b183cc504b5d9ab323c0bb2d4c1ea6a116a5d588
Matthew Opperman <mattopp17@gmail.com>
2016-05-16 02:53:24 -0400

    add TC/Optimization info

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,6 +28,17 @@ BOARD_HAVE_BLUETOOTH_BCM := true
 TARGET_KERNEL_CONFIG := coocoo_f400_defconfig
 TARGET_REQUIRES_BUMP := true
 
+# Kernel Toolchain
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-linux-androideabi-4.9-uber/bin
+KERNEL_TOOLCHAIN_PREFIX := arm-linux-androideabi-
+
+# Optimizations
+CLANG_O3 := true
+STRICT_ALIASING := false
+KRAIT_TUNINGS := true
+GRAPHITE_OPTS := false
+ENABLE_GCCONLY := true
+
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
