db2b72af3fb7d3e410314a79b5acb04345a07744
oadam11 <ovadia.adam@gmail.com>
2016-10-07 10:18:50 -0400

    vs985: add back gps

--- device.mk
+++ device.mk
@@ -39,6 +39,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libnetcmdiface
 
+# GPS
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf
+
 # NFC packages
 PRODUCT_PACKAGES += \
     NfcNci \
