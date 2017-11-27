4c2ab77416b772a629e75fc0a31007eba401aec5
oadam11 <ovadia.adam@gmail.com>
2017-11-27 13:43:39 -0500

    validus'ify

--- validus.mk
+++ validus.mk
@@ -1,11 +1,12 @@
 $(call inherit-product, device/lge/f400/full_f400.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/gzosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/validus/config/common_full_phone.mk)
 
-PRODUCT_NAME := gzosp_f400
+PRODUCT_NAME := validus_f400
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
+    DEVICE_MAINTAINERS="Adam Ovadia (oadam11)" \
     PRODUCT_DEVICE="g3" \
     PRODUCT_NAME="g3_kt_kr" \
     BUILD_FINGERPRINT="lge/g3_kt_kr/g3:6.0/MRA58K/1606909301116:user/release-keys" \
